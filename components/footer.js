d95a4f877ceb6ca283868bc8cdb0f93a176d9bfc
Masi Tashakori <masitashakori@gmail.com>
2019-09-12 22:28:46 +1000

    add components

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+import react from 'react'
+
+const Footer = () => (
+    <footer className="footer bg-dark">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6">
+                <p className="copyright font-alt">&copy; 2017&nbsp;<a href="index.html">Persian Hand Embroidered Accessories</a>, All Rights Reserved</p>
+              </div>
+              <div className="col-sm-6">
+                <div className="footer-social-links"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-dribbble"></i></a><a href="#"><i className="fa fa-skype"></i></a>
+                </div>
+              </div>
+            </div>
+        </div>
+    </footer>
+)
+
+export default Footer
---
+++ components/latest.js
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import { withRouter } from "next/router"
+
+const Latest = (props) => {
+    const imgSrc = './static/theme/assets/images/shop/' + props.imgName
+    const imgAlt = props.alt || 'Accessories Pack'
+    
+    return (
+        <div className="col-sm-6 col-md-3 col-lg-3">
+            <div className="shop-item">
+                <div className="shop-item-image"><img src={imgSrc} alt={imgAlt}/>
+                <div className="product__price-button-container">        
+                </div>
+                <div className="shop-item-detail">
+                  <span className="btn-round">
+                      <button 
+                        className="snipcart-add-item product__button btn btn-b"
+                        data-item-id={props.id}
+                        data-item-name={props.alt}
+                        data-item-price={props.price}
+                        data-item-image={imgSrc}
+                        data-item-description={props.desc}>
+                          <span className="icon-basket">
+                             Add To Cart </span>
+                      </button>
+                    </span>
+                </div>
+            </div>
+            <h4 className="shop-item-title font-alt">
+              <Link href="/single_product/[id]" as={`/single_product/${props.id}`}>
+                <a>{imgAlt}</a>
+              </Link>
+            </h4>
+            {props.price}
+            
+          </div>
+          
+
+        </div>
+    )
+}
+
+export default withRouter(Latest)
